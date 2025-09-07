33e7d0538672b56bf79db24bfdc283faebd211e1
TessierAshpoule256 <tessier_ashpoule_2135@list.ru>
2025-09-07 09:10:34 +0300

    ref

--- feature/course_list/src/main/java/org/appcourse/course_list/ui/view/BottomNavBar.kt
+++ feature/course_list/src/main/java/org/appcourse/course_list/ui/view/BottomNavBar.kt
@@ -1,11 +1,18 @@
 package org.appcourse.course_list.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.appcourse.navigation.FakeNavigator
 import org.appcourse.navigation.NavContract
 
 
@@ -23,10 +30,27 @@ fun BottomNavBar(
     }
 
     Row(
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier
+            .height(72.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
     ) {
         buttonList.forEach {
             DrawBottomNavButton(it)
         }
+    }
+}
+
+@Preview(
+    name = "Light Theme",
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 200,
+    backgroundColor = 0xFFFFFFFF
+)
+@Composable
+private fun BottomNavBarPreview() {
+    MaterialTheme {
+        BottomNavBar(FakeNavigator())
     }
 }
