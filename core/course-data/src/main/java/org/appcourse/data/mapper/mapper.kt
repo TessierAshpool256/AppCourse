78f6c3fc430826db10b8d49329db112e6e1193ba
TessierAshpoule256 <tessier_ashpoule_2135@list.ru>
2025-09-10 17:18:52 +0300

    add data mapper

---
+++ core/course-data/src/main/java/org/appcourse/data/mapper/mapper.kt
@@ -0,0 +1,32 @@
+package org.appcourse.data.mapper
+
+import org.appcourse.cours_api.models.CourseDTO
+import org.appcourse.course_list.models.CourseEntity
+import org.appcourse.db.models.CourseDBO
+
+
+fun CourseDBO.toCourseEntity(): CourseEntity {
+    return CourseEntity(
+        id = this.id?.toLong() ?: 0,
+        title = this.title.orEmpty(),
+        text = this.text.orEmpty(),
+        price = this.price.orEmpty(),
+        rate = this.rate.orEmpty(),
+        startDate = this.startDate.orEmpty(),
+        hasLike = this.hasLike ?: false,
+        publishDate = this.publishDate.orEmpty()
+    )
+}
+
+fun CourseDTO.toCourseDBO(): CourseDBO {
+    return CourseDBO(
+        id = this.id ?: 0,
+        title = this.title.orEmpty(),
+        text = this.text.orEmpty(),
+        price = this.price.orEmpty(),
+        rate = this.rate.orEmpty(),
+        startDate = this.startDate.orEmpty(),
+        hasLike = this.hasLike ?: false,
+        publishDate = this.publishDate.orEmpty()
+    )
+}
