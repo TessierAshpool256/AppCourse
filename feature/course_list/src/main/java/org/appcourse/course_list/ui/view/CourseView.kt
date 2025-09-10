ec107a8803e35c0e63b6d8126ba4e152509b7dd8
TessierAshpoule256 <tessier_ashpoule_2135@list.ru>
2025-09-10 20:16:15 +0300

    dev CourseView

--- feature/course_list/src/main/java/org/appcourse/course_list/ui/view/CourseView.kt
+++ feature/course_list/src/main/java/org/appcourse/course_list/ui/view/CourseView.kt
@@ -7,23 +7,34 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import org.appcourse.course_list.R
 import org.appcourse.course_list.models.CourseEntity
+import org.appcourse.course_list.utile.mockCourse
 import org.appcourse.course_list.utile.mockImageByCourseId
 import org.appcourse.ui_theme.AppCourseTheme
+import org.appcourse.ui_theme.Glass
 
 
 @Composable
@@ -32,57 +43,139 @@ fun CourseView(
 ) {
     Box(
         modifier = Modifier
-            .clip(MaterialTheme.shapes.extraLarge)
+            .clip(MaterialTheme.shapes.medium)
             .background(MaterialTheme.colorScheme.tertiary)
             .fillMaxWidth(.9f),
     ) {
-        ConstraintLayout(
-
-        ) {
-            val (rate, date, image, bookmark) = createRefs()
-            Image(
-                modifier = Modifier
-                    .constrainAs(image) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    }
-                    .fillMaxWidth(1f)
-                ,
-                contentScale = ContentScale.Crop,
-                painter = painterResource(mockImageByCourseId(course.id)),
-                contentDescription = "Course logo"
-            )
-        }
-
         Column {
-            Text(
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleMedium,
-                text = course.title
-            )
+            ConstraintLayout(
 
-            Text(
-                course.text
-            )
-
-            Row(
-                modifier = Modifier.fillMaxWidth(1f),
-                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    modifier = Modifier.padding(start = 16.dp),
-                    text = course.price
+                val (rate, date, image, bookmark) = createRefs()
+                Image(
+                    modifier = Modifier
+                        .constrainAs(image) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
+                        .fillMaxWidth(1f)
+                    ,
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(mockImageByCourseId(course.id)),
+                    contentDescription = "Course logo"
                 )
 
-                Text(
-                    modifier = Modifier.padding(end = 16.dp),
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodySmall,
-                    text = "Подробнее →"
-                )
+                Box(
+                    modifier = Modifier
+                        .size(28.dp)
+                        .constrainAs(bookmark) {
+                            top.linkTo(parent.top, margin = 8.dp)
+                            end.linkTo(parent.end, margin = 8.dp)
+                        }
+                        .background(Glass, shape = CircleShape)
+                        .aspectRatio(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Box (
+                        modifier = Modifier
+                            .blur(radius = 16.dp)
+                    )
+                    Icon(
+                        modifier = Modifier
+                            .size(16.dp),
+                        painter = painterResource(R.drawable.ic_bookmark),
+                        contentDescription = "Добавить в избранное",
+                        tint = MaterialTheme.colorScheme.secondary
+                    )
+                }
+
+                Row(
+                    modifier = Modifier
+                        .size(width = 46.dp, height = 22.dp)
+                        .constrainAs(rate) {
+                            start.linkTo(parent.start, margin = 8.dp)
+                            bottom.linkTo(parent.bottom, margin = 8.dp)
+                        }
+                        .background(Glass, shape = MaterialTheme.shapes.extraLarge),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(12.dp),
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "Star",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+
+                    Text(
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.bodySmall,
+                        text = course.rate
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .height(22.dp)
+                        .constrainAs(date) {
+                            start.linkTo(rate.end, margin = 8.dp)
+                            bottom.linkTo(parent.bottom, margin = 8.dp)
+                        }
+                        .background(Glass, shape = MaterialTheme.shapes.extraLarge),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.bodySmall,
+                        text = course.publishDate
+                    )
+                }
             }
-            Spacer(modifier = Modifier.padding(8.dp))
+
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.titleMedium,
+                        text = course.title
+                    )
+
+                    Text(
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.bodySmall,
+                        maxLines = 2,
+                        text = course.text
+                    )
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(1f),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            color = MaterialTheme.colorScheme.secondary,
+                            modifier = Modifier.padding(start = 16.dp),
+                            text = "${course.price} ₽"
+                        )
+
+                        Text(
+                            modifier = Modifier.padding(end = 16.dp),
+                            color = MaterialTheme.colorScheme.primary,
+                            style = MaterialTheme.typography.bodySmall,
+                            text = "Подробнее →"
+                        )
+                    }
+                    Spacer(modifier = Modifier.padding(8.dp))
+                }
+            }
         }
     }
 
@@ -92,16 +185,8 @@ fun CourseView(
 @Preview(device = "spec:width=1080px,height=2422px")
 @Composable
 fun previewCourseView() {
-    val course = CourseEntity(
-            100,
-            "Java-разработчик с нуля",
-            "Освойте backend-разработку и программирование на Java, фреймворки Spring и Maven, работу с базами данных и API. Создайте свой собственный проект, собрав портфолио и став востребованным специалистом для любой IT компании.",
-            "999",
-            "4.9",
-            "2024-05-22",
-            false,
-            "2024-02-02"
-    )
+    val course = mockCourse()
+
     AppCourseTheme {
         Column(
             modifier = Modifier
