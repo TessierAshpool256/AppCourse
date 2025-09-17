1af48990b8e096b90025f399d4ceb7fef14354bf
TessierAshpoule256 <tessier_ashpoule_2135@list.ru>
2025-09-17 08:48:01 +0300

    ref

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
 
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kapt) apply false
 
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.androidx.room) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
--- feature/course/ui/src/main/java/org/appcourse/course/ui/ui/view/getNavList.kt
+++
@@ -1,6 +0,0 @@
-package org.appcourse.course.ui.ui.view
-
-import androidx.compose.runtime.mutableStateOf
-import org.appcourse.course_list.R
-
-
